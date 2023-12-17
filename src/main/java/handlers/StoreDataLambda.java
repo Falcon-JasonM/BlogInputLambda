67a152e33e1036bb67df3be5fd070a0acd50e774
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2023-12-16 19:41:08 -0600

    "refactored to work with API Gateway"

--- src/main/java/handlers/StoreDataLambda.java
+++ src/main/java/handlers/StoreDataLambda.java
@@ -3,8 +3,12 @@ package handlers;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
@@ -13,9 +17,7 @@ import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRespon
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -29,46 +31,64 @@ public class StoreDataLambda implements RequestStreamHandler {
         ObjectMapper mapper = new ObjectMapper();
         Connection connection = null;
         LambdaLogger LOGGER = context.getLogger();
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = new JSONObject();
 
         try {
-            String secretName = System.getenv("SECRET_NAME");
-            Region region = Region.of("us-east-2");
+            JSONObject event = (JSONObject) parser.parse(new InputStreamReader(input));
 
-            SecretsManagerClient client = SecretsManagerClient.builder()
-                    .region(region)
-                    .build();
+            if (event.get("body") != null ) {
+                JSONObject body = (JSONObject) parser.parse(event.get("body").toString());
 
-            GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-                    .secretId(secretName)
-                    .build();
+                LOGGER.log("Body: " + body.toJSONString(), LogLevel.INFO);
 
-            GetSecretValueResponse getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
-            String secret = getSecretValueResponse.secretString();
-            JsonNode secretJson = mapper.readTree(secret);
-            String dbUsername = secretJson.get("username").asText();
-            String dbPassword = secretJson.get("password").asText();
+                String title = body.get("title").toString();
+                String content = body.get("content").toString();
 
-            LOGGER.log("Attempting to connect to the DB...\n", LogLevel.INFO);
-            connection = DriverManager.getConnection(DATABASE_URL, dbUsername, dbPassword);
+                String secretName = System.getenv("SECRET_NAME");
+                Region region = Region.of("us-east-2");
 
-            JsonNode jsonInput = mapper.readTree(input);
+                SecretsManagerClient client = SecretsManagerClient.builder()
+                        .region(region)
+                        .build();
 
-            // Assuming JSON structure: { "title": "value1", "content": "value2" }
-            String title = jsonInput.get("title").asText();
-            String content = jsonInput.get("content").asText();
+                GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                        .secretId(secretName)
+                        .build();
 
-            // Store data into PostgreSQL using prepared statement
-            String sql = "INSERT INTO blog_page.blog_post (title, content) VALUES (?, ?)";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, title);
-            statement.setString(2, content);
-            statement.executeUpdate();
+                GetSecretValueResponse getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+                String secret = getSecretValueResponse.secretString();
+                JsonNode secretJson = mapper.readTree(secret);
+                String dbUsername = secretJson.get("username").asText();
+                String dbPassword = secretJson.get("password").asText();
 
-            // Close resources
-            statement.close();
+                LOGGER.log("Attempting to connect to the DB...\n", LogLevel.INFO);
+                connection = DriverManager.getConnection(DATABASE_URL, dbUsername, dbPassword);
+
+                String sql = "INSERT INTO blog_page.blog_post (title, content) VALUES (?, ?)";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, title);
+                statement.setString(2, content);
+                statement.executeUpdate();
+                statement.close();
+
+                APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+                String responseBody = "Blog post successfully added";
+                responseEvent.setStatusCode(200);
+                responseEvent.setBody(responseBody);
+
+                try (OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8")) {
+                    writer.write(mapper.writeValueAsString(responseEvent));
+                }
+            } else {
+                LOGGER.log("Request body is empty or missing", LogLevel.ERROR);
+            }
         } catch (SQLException e) {
             LOGGER.log("Error occurred while connecting to the database: \n" + e.getMessage(), LogLevel.ERROR);
             e.printStackTrace();
+        } catch (ParseException | IOException e) {
+            LOGGER.log("Error occurred while parsing the request body: \n" + e.getMessage(), LogLevel.ERROR);
+            throw new RuntimeException(e);
         } finally {
             try {
                 if (connection != null && !connection.isClosed()) {
