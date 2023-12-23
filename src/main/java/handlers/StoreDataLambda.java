6c0b3c1b9e46fbdfac75455d8287ad96e363a111
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2023-12-22 18:55:00 -0600

    "changed options call handler"

--- src/main/java/handlers/StoreDataLambda.java
+++ src/main/java/handlers/StoreDataLambda.java
@@ -7,6 +7,10 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
@@ -32,23 +36,41 @@ public class StoreDataLambda implements RequestStreamHandler {
         ObjectMapper mapper = new ObjectMapper();
         Connection connection = null;
         LambdaLogger LOGGER = context.getLogger();
+        JSONParser parser = new JSONParser();
         APIGatewayV2HTTPResponse responseEvent = new APIGatewayV2HTTPResponse();
         APIGatewayV2HTTPEvent requestEvent;
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("Access-Control-Allow-Origin", "*");
-        headers.put("Access-Control-Allow-Methods", "GET, OPTIONS");
+        headers.put("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, OPTIONS");
         headers.put("Access-Control-Allow-Headers", "Content-Type");
 
         try {
-           requestEvent = mapper.readValue(input, APIGatewayV2HTTPEvent.class);
-
+                // Parse the received JSON content into an APIGatewayV2HTTPEvent object
+                JSONObject event;
+                try {
+                    event = (JSONObject) parser.parse(new InputStreamReader(input));
+                    LOGGER.log("Received event: " + event, LogLevel.DEBUG);
+                } catch (IOException | ParseException ex) {
+                    LOGGER.log("Error parsing input event into JSON format: " + ex.getMessage(), LogLevel.ERROR);
+                    throw new RuntimeException(ex);
+                }
+                try {
+                    requestEvent = mapper.readValue(event.toJSONString(), APIGatewayV2HTTPEvent.class);
+                } catch (JsonProcessingException ex) {
+                    LOGGER.log("Error parsing APIGatewayV2HTTPEvent: " + ex.getMessage(), LogLevel.ERROR);
+                    throw new RuntimeException(ex);
+                }
+                LOGGER.log("Parsed APIGatewayV2HTTPEvent: " + requestEvent, LogLevel.DEBUG);
             // Return http status = 200 OK if request method is OPTIONS
-            if (requestEvent.getHeaders() != null && requestEvent.getHeaders().containsKey("httpMethod") &&
-                    "OPTIONS".equals(requestEvent.getHeaders().get("httpMethod"))) {
+            if ((requestEvent!= null && requestEvent.getHeaders().containsKey("httpMethod") &&
+                    "OPTIONS".equals(requestEvent.getHeaders().get("httpMethod")))||requestEvent.getRouteKey().contains("OPTIONS")) {
                 responseEvent.setStatusCode(200);
                 responseEvent.setHeaders(headers);
+                String responseBody = "{\"message\": \"OK\"}";
+                responseEvent.setBody(responseBody);
                 responseEvent.setIsBase64Encoded(false);
+                LOGGER.log("Response to pre-flight check: " + responseEvent, LogLevel.DEBUG);
                 try {
                     mapper.writeValue(output, responseEvent);
                 } catch (IOException ex) {
@@ -102,6 +124,11 @@ public class StoreDataLambda implements RequestStreamHandler {
                 mapper.writeValue(output,responseEvent);
             } else {
                 LOGGER.log("Request body is empty or missing", LogLevel.ERROR);
+                responseEvent = new APIGatewayV2HTTPResponse();
+                responseEvent.setStatusCode(400);
+                responseEvent.setBody("\"Request body is empty or missing\"");
+                responseEvent.setIsBase64Encoded(false);
+                mapper.writeValue(output, responseEvent);
             }
         } catch (SQLException e) {
             LOGGER.log("Error occurred while connecting to the database: \n" + e.getMessage(), LogLevel.ERROR);
